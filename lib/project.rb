24015f5b223468c6eba87bee610130af4c5f4563
Richell Bonfield <richell.bonfield12@gmail.com>
2019-09-29 03:01:18 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -6,8 +6,8 @@ class Project
     @backers = []
   end 
   
-  def add_backer(project_name)
-    backers << project_name
-    project_name.backed_projects << self
+  def add_backer(backer)
+    backers << backer
+    backer.backed_projects << self
   end
 end

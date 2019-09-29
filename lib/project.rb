87b581c9047de94866b6b05c67826e99a0829a84
Richell Bonfield <richell.bonfield12@gmail.com>
2019-09-29 02:56:55 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,11 +1,13 @@
 class Project 
-  attr_reader :backers
+  attr_reader :backers, :name
   
   def initialize
+    @name = name
     @backers = []
   end 
   
   def add_backer(project_name)
-    self.backers << project_name
+    backers << project_name
+    backers.backed_projects << self
   end
 end
